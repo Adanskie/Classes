7ec66c5a5fbb6100ee6d3022df233578e3319a34
Adanielle Sabaria <155322408+Adanskie@users.noreply.github.com>
2024-11-09 23:12:50 +0800

    Practice_Classes_1.py
    Practice classes

---
+++ Classes_1.py
@@ -0,0 +1,7 @@
+class Name:
+  def __init__(self,name,lastname):
+    self.name = name
+    self.lastname = lastname
+
+get_name = Name("adanielle","Sabaria")
+print(f"Your name is {get_name.name} {get_name.lastname}")
